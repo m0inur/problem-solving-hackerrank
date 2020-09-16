5b87e23bd82ef4cd4fa8a40f8236292b0af68d70
moinur-najib <moinurnajib@gmail.com>
2020-09-16 17:21:47 +0600

    String Constructor - Problem Solving

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-function funnyString(string) {
-    var original = [];
-    var org = [];
-    var reversed = [];
-
-    var originalDiff = [];
-    var reversedDiff = [];
-    var isMatched = true;
+function stringConstruction(string) {
+    var found = [];
+    var append = 0;
 
     for (var i = 0; i < string.length; i++) {
-        original.push(string[i].charCodeAt(0));
-        org.push(string[i].charCodeAt(0));
-    }
-
-    reversed = original.reverse();
-
-    for (var i = 0; i < org.length - 1; i++) {
-        originalDiff.push(Math.abs(org[i] - org[i + 1]));
-        reversedDiff.push(Math.abs(reversed[i] - reversed[i + 1]));
-    }
-
-    for (var i = 0; i < org.length - 1; i++) {
-        if (originalDiff[i] != reversedDiff[i]) {
-            isMatched = false;
+        if (found.indexOf(string[i]) == -1) {
+            found.push(string[i]);
+            append++;
         }
     }
 
-    if (isMatched) {
-        return "Funny";
-    } else {
-        return "Not Funny";
-    }
+    return append;
 }

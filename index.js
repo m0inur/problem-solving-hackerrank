fd39557818d948d7e66bda08d03ebd9050f81428
moinur-najib <moinurnajib@gmail.com>
2020-09-16 14:14:02 +0600

    Pangram - Problem solving

--- index.js
+++ index.js
@@ -1,56 +1,20 @@
-function caesarCipher(str, k) {
-    var lowercaseRegex = RegExp(/[a-z]/);
-    var highercaseRegex = RegExp(/[A-Z]/);
-    var newStr = "";
-    var index = 0;
-
-    // letter generator start and end points
-    var start = 'a'.charCodeAt(0);
-    var last = 'z'.charCodeAt(0);
-
-    // Letter maps
-    var orgLettersArr = [];
-    var changedLetters = new Map();
-    var orgLetters = new Map();
-
-    // Counters
-    var keyCounter = 0;
-    var counter = 0;
-    var counter2 = 0;
-    var iterate = 0;
-
-    // Generate Alphabets
-
-    for (var i = start; i <= last; i++) {
-        orgLettersArr.push(String.fromCharCode(i));
-    }
-
-    for (var i = 0; i < k; i++) {
-        index = orgLettersArr[0];
-        orgLettersArr.splice(0, 1);
-        orgLettersArr.push(index);
-    }
-
-    for (var i = start; i <= last; i++) {
-        keyCounter++;
-        changedLetters.set(keyCounter, orgLettersArr[iterate]);
-        orgLetters.set(String.fromCharCode(i), keyCounter);
-        iterate++;
-    }
+function pangrams(str) {
+    // Turn string into all lowercase and remove whitespaces
+    str = str.toLowerCase();
+    str = str.replace(/ /g, '');
+    var found = [];
 
     for (var i = 0; i < str.length; i++) {
-        if (lowercaseRegex.test(str[i])) {
-            newStr += changedLetters.get(orgLetters.get(str[i]));
-        }
-
-        if (highercaseRegex.test(str[i])) {
-            newStr += changedLetters.get(orgLetters.get(str[i].toLowerCase())).toUpperCase();
-        }
-
-        if (!lowercaseRegex.test(str[i]) && !highercaseRegex.test(str[i])) {
-            newStr += str[i];
+        if (found.indexOf(str[i]) < 0) {
+            found.push(str[i]);
+        } else {
+            str = str.slice(0, i) + str.slice(i);
         }
     }
 
-    return newStr;
+    if (found.length == 26) {
+        return "pangram";
+    } else {
+        return "not pangram";
+    }
 }

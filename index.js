d4f1c1fb425cb007a65f1ed2482671f31a3b692a
moinur-najib <moinurnajib@gmail.com>
2020-09-17 07:40:38 +0600

    Marc's Cakewalk - Problem solving

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-function stringConstruction(string) {
-    var found = [];
-    var append = 0;
+function isSorted(a) {
+    for (var x = 0; x < a.length; x++) {
+        if (a[x] < a[x + 1]) {
+            return false;
+        }
 
-    for (var i = 0; i < string.length; i++) {
-        if (found.indexOf(string[i]) == -1) {
-            found.push(string[i]);
-            append++;
+        if (x == a.length - 1) {
+            return true;
         }
     }
+}
 
-    return append;
+function mySort(a) {
+    var index = 0;
+    var sorted = false;
+    var i = 0;
+    while (!sorted) {
+        i++;
+
+        if (a[i] < a[i + 1]) {
+            index = a[i];
+            a.splice(i, 1, a[i + 1]);
+            a.splice(i + 1, 1, index);
+        }
+
+        if (i == a.length - 1) {
+            if (isSorted(a)) {
+                return a;
+            } else {
+                i = -1;
+            }
+        }
+    }
+}
+
+function marcsCakewalk(calories) {
+    mySort(calories);
+    var result = 0;
+    var eaten = 0;
+
+    for (var i = 0; i < calories.length; i++) {
+        result += Math.pow(2, eaten) * calories[i];
+        eaten++;
+    }
+
+    return result;
 }

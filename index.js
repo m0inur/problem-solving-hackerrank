e28ed8fce04542d0beae8fbe232ed6ec30daf902
moinur-najib <moinurnajib@gmail.com>
2020-09-17 17:38:13 +0600

    Jumping on the clouds revisited - Problem solving

--- index.js
+++ index.js
@@ -1,47 +1,14 @@
-function isSorted(a) {
-    for (var x = 0; x < a.length; x++) {
-        if (a[x] < a[x + 1]) {
-            return false;
-        }
+function jumpingOnClouds(c, k) {
+    var energy = 100;
+    var n = c.length;
+    var i = k % n;
+    energy -= c[i] * 2 + 1;
 
-        if (x == a.length - 1) {
-            return true;
-        }
-    }
-}
-
-function mySort(a) {
-    var index = 0;
-    var sorted = false;
-    var i = 0;
-    while (!sorted) {
-        i++;
-
-        if (a[i] < a[i + 1]) {
-            index = a[i];
-            a.splice(i, 1, a[i + 1]);
-            a.splice(i + 1, 1, index);
-        }
-
-        if (i == a.length - 1) {
-            if (isSorted(a)) {
-                return a;
-            } else {
-                i = -1;
-            }
-        }
-    }
-}
-
-function marcsCakewalk(calories) {
-    mySort(calories);
-    var result = 0;
-    var eaten = 0;
-
-    for (var i = 0; i < calories.length; i++) {
-        result += Math.pow(2, eaten) * calories[i];
-        eaten++;
+    while (i != 0) {
+        i = (i + k) % n;
+        energy -= c[i] * 2 + 1;
     }
 
-    return result;
+
+    return energy
 }

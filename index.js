4b878454066c33fa4285d5a45efb98691ea9772a
moinur-najib <moinurnajib@gmail.com>
2020-09-23 07:00:26 +0600

    Drawing book - Problem solving

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-function circularArrayRotation(arr, k, queries) {
-    var newArr = [];
+function pageCount(n, p) {
+    var pages = [];
+    var book = [];
+    var flipped = -1;
+    var i = 1;
+    var failsafe = 0;
 
-    for (var i = 0; i < k; i++) {
-        arr.unshift(arr[arr.length - 1]);
-        arr.splice(arr.length - 1, 1);
+    var forwardCounter = 0;
+    var backwardCounter = 0;
+    var foundReversed = false;
+
+    for (i = 1; pages[0] != n; i += 2) {
+        if (pages[1] != n) {
+            pages = [];
+            pages.push(i - 1);
+            pages.push(i);
+
+            book.push(pages);
+        } else {
+            break;
+        }
     }
 
-    for (var i = 0; i < queries.length; i++) {
-        newArr.push(arr[queries[i]]);
+    var i = 0
+
+    console.log(book);
+    console.log();
+
+    for (i = 0; i < book.length; i++) {
+        if (book[i].indexOf(p) > -1) {
+            forwardCounter = i;
+        }
+
+        if (book[book.length - (i + 1)].indexOf(p) > -1) {
+            foundReversed = true;
+        }
+
+        if (foundReversed == false) {
+            backwardCounter++;
+        }
     }
 
-    return newArr;
+    if (forwardCounter < backwardCounter) {
+        return forwardCounter;
+    } else {
+        return backwardCounter;
+    }
 }

58a9359b41e0167e5d5dbd001aae7dade07babd5
moinur-najib <moinurnajib@gmail.com>
2020-09-23 09:49:31 +0600

    Lisa's Workbook - Problem solving

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
-function serviceLane(width, cases) {
-    const result = []
-    for (let c of cases) {
-        const slicedArr = width.slice(c[0], c[1] + 1)
-        const min = Math.min(...slicedArr)
-        result.push(min)
-    }
-    return result
-}
+// Complete the workbook function below.
+function workbook(chapters, probPerPage, arr) {
+    var page = 0;
+    var specialProbs = 0;
 
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+    for (var i = 1; i <= chapters; i++) {
+        page++;
 
-    const nt = readLine().split(' ');
+        for (var j = 1; j <= arr[i - 1]; j++) {
+            if (page == j) {
+                specialProbs++;
+            }
 
-    const n = parseInt(nt[0], 10);
-
-    const t = parseInt(nt[1], 10);
-
-    const width = readLine().split(' ').map(widthTemp => parseInt(widthTemp, 10));
-
-    let cases = Array(t);
-
-    for (let i = 0; i < t; i++) {
-        cases[i] = readLine().split(' ').map(casesTemp => parseInt(casesTemp, 10));
+            if (j % probPerPage == 0 && j != arr[i - 1]) {
+                page++;
+            }
+        }
     }
 
-    let result = serviceLane(width, cases);
-
-    ws.write(result.join("\n") + "\n");
-
-    ws.end();
+    return specialProbs;
 }

70c05282b696d8973b59936e577da4f2e45ad091
moinur-najib <moinurnajib@gmail.com>
2020-10-06 13:51:13 +0600

    Picking Numbers - Problem Solving

--- Problem-Solving/Picking-numbers/index.js
+++ Problem-Solving/Picking-numbers/index.js
@@ -1,60 +1,27 @@
-var arr = [5, 4, 3, 2];
+function pickingNumbers(arr) {
+    var num = -1;
+    var len = 0;
+    var maxLen = 0;
 
-function isSorted(a) {
-    for (var x = 0; x < a.length; x++) {
-        if (a[x] > a[x + 1]) {
-            return false;
+    arr.sort((a, b) => a - b);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (num < 0) {
+            num = arr[i];
         }
 
-        if (x == a.length - 1) {
-            return true;
-        }
-    }
-}
-
-function sortArr(arr) {
-    var a = arr;
-
-    var i = 0;
-    var sorted = false;
-    var swaps = 0;
-
-    while (!sorted) {
-        i++;
-
-        if (a[i] > a[i + 1]) {
-            var index = a[i];
-            a.splice(i, 1, a[i + 1]);
-            a.splice(i + 1, 1, index);
-
-            swaps++;
-        }
-
-        if (i == a.length - 1) {
-            if (isSorted(a)) {
-                break;
-            } else {
-                i = -1;
+        if (arr[i + 1] - num <= 1) {
+            len++;
+        } else {
+            len++;
+            if (maxLen < len) {
+                maxLen = len;
             }
+
+            num = -1;
+            len = 0;
         }
     }
-    return a;
+
+    return maxLen;
 }
-
-var anotherArr = [];
-
-arr = sortArr(arr)
-
-// console.log(arr);
-
-for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr.length; j++) {
-        if (arr[i] - arr[j] == 1) {
-            // console.log("(" + arr[j] + "," + arr[i] + ")");
-            anotherArr.push(arr[j])
-            anotherArr.push(arr[i])
-        }
-    }
-}
-
-console.log(anotherArr.join(" "));
